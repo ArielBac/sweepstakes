415f8b2fa6fa6c76a17e6dd10e6694105c363950
ArielBac <arielvieira65@gmail.com>
2022-10-18 09:45:58 -0300

    Button fix on register page

--- resources/views/sweepstakes/register.blade.php
+++ resources/views/sweepstakes/register.blade.php
@@ -76,7 +76,7 @@
                         @enderror
                     </div>
 
-                    <div class="w-full bg-gray-50 px-4 py-3 text-right sm:px-6">
+                    <div class="w-fit bg-gray-50 py-3 text-right">
                         <button type="submit"
                             class=" w-full inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                             Participar
